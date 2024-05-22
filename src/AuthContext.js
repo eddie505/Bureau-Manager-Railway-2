c6786ac4158a96a576f30970f4fe3ebf07621ad1
eddie <124751851+eddie505@users.noreply.github.com>
2024-05-21 18:37:37 -0600

    Hosting

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,6 +1,5 @@
-import React, { createContext, useContext, useState } from 'react';
-
-
+/* eslint-disable */
+import React, { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
@@ -11,24 +10,22 @@ export function useAuth() {
 export const AuthProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState(() => {
     // Intenta obtener el token del localStorage
-    const localData = localStorage.getItem('authData');
+    const localData = localStorage.getItem("authData");
     return localData ? JSON.parse(localData).token : null;
   });
 
   // Función para iniciar sesión y guardar el token
   const login = (token, id) => {
     setAuthToken(token);
-    localStorage.setItem('authData', JSON.stringify({ token, id }));
+    localStorage.setItem("authData", JSON.stringify({ token, id }));
   };
 
   // Función para cerrar sesión
   const logout = () => {
     setAuthToken(null);
-    localStorage.removeItem('authData');
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authData");
+    localStorage.removeItem("authToken");
   };
-
-  
 
   const value = {
     authToken,

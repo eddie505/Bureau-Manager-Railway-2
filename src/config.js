1d8eed153f5873996636ea15c03aff0a6a1ef8b7
eddie <124751851+eddie505@users.noreply.github.com>
2024-05-21 23:04:30 -0600

    Hosting:>

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 7110;
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "n0m3l0";
 export const DB_USER = process.env.DB_USER || "root";

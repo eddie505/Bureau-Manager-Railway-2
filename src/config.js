de6e16fe9ec9f5e5252f11f248115cedb1f18e99
eddie <124751851+eddie505@users.noreply.github.com>
2024-05-27 20:38:31 -0600

    ENV3

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
-export const PORT = process.env.PORT || 4000;
-export const DB_HOST = process.env.DB_HOST || "monorail.proxy.rlwy.net:57372";
-export const DB_PASSWORD =
-  process.env.DB_PASSWORD || "gTiWfXvJzZwpOyRADjjhknTGPESchiuZ";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_NAME = process.env.DB_NAME || "railway";
-export const DB_PORT = process.env.DB_PORT || 3306;
-export const SECRET_KEY = process.env.SECRET_KEY || "secret_key";
+export const PORT = 4000;
+export const DB_HOST = "monorail.proxy.rlwy.net";
+export const DB_PASSWORD = "gTiWfXvJzZwpOyRADjjhknTGPESchiuZ";
+export const DB_USER = "root";
+export const DB_NAME = "railway";
+export const DB_PORT = 57372;
+export const SECRET_KEY = "secret_key";
 export const REACT_APP_SERVER_URL =
-  process.env.REACT_APP_SERVER_URL ||
   "https://bureau-manager-railway-2-production.up.railway.app";

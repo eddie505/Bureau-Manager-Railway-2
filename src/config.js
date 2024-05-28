b3ee694163d4ac94ea189eaaeb6cc298192e9073
eddie <124751851+eddie505@users.noreply.github.com>
2024-05-27 19:32:25 -0600

    ENV_CHN

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 export const PORT = process.env.PORT || 4000;
 export const DB_HOST = process.env.DB_HOST || "monorail.proxy.rlwy.net:57372";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "n0m3l0";
+export const DB_PASSWORD =
+  process.env.DB_PASSWORD || "gTiWfXvJzZwpOyRADjjhknTGPESchiuZ";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_NAME = process.env.DB_NAME || "bureau-manager";
+export const DB_NAME = process.env.DB_NAME || "railway";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const SECRET_KEY = process.env.SECRET_KEY || "secret_key";
 export const REACT_APP_SERVER_URL =
